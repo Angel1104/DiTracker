82e28b757213aa183c8b54825df4b897a10639e1
Angel1104 <flores1102angel266@gmail.com>
2023-09-26 15:53:43 -0400

    inicio

---
+++ src/main/java/com/dh/ditracker/DitrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dh.ditracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DitrackerApplication.class, args);
+	}
+
+}
