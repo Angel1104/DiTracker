32a3491410124e66042561206ce25883c431c225
Angel1104 <flores1102angel266@gmail.com>
2023-09-26 23:02:33 -0400

    security model advance table user

--- src/main/java/com/dh/ditracker/model/domain/security/UserEntity.java
+++ src/main/java/com/dh/ditracker/model/domain/security/UserEntity.java
@@ -1,4 +1,28 @@
 package com.dh.ditracker.model.domain.security;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "user_")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "id_", nullable = false)
+    private Integer id;
+    @Column(name = "userName_")
+    private String userName;
+    @Column(name = "password_")
+    private String password;
+    @Column(name = "name_")
+    private String name;
+    @Column(name = "lastName_")
+    private String lastName;
+    @Column(name = "avatarId_")
+    private Integer avatarId;
+    @Column(name = "email_")
+    private String email;
 }
