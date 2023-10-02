d4444d578d26889cb8fb01291d7dec5673ce69a6
Angel1104 <flores1102angel266@gmail.com>
2023-10-02 08:44:18 -0400

    new files in api

--- src/main/java/com/dh/ditracker/command/role/UpdateUserCommand.java
+++ src/main/java/com/dh/ditracker/command/role/UpdateUserCommand.java
@@ -1,5 +1,8 @@
 package com.dh.ditracker.command.role;
 
+import com.dh.ditracker.command.AbstractCommand;
+import com.dh.ditracker.model.mapper.UserMapper;
+import com.dh.ditracker.service.UserService;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -12,5 +15,24 @@ import org.springframework.stereotype.Service;
  */
 @Slf4j
 @Service
-public class UpdateUserCommand {
+public final class UpdateUserCommand extends AbstractCommand {
+    @Setter
+    private UpdateUserRequest input;
+    @Getter
+    private UpdateUserResponse output;
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UpdateUserCommand (UserService userService, UserMapper userMapper){
+        this.userService = userService;
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    protected void onExecute(){
+        log.info("UpdateUserCommand - Execute");
+        //UserResponse
+    }
 }
---
+++ src/main/java/com/dh/ditracker/model/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.dh.ditracker.model.mapper;
+
+public class UserMapper {
+}
